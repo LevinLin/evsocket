b42be416d3c65b0c759f3e5f632cfc139474cd02
LevinLin <vllm@163.com>
2015-04-28 21:16:49 +0800

    master

---
+++ stats.h
@@ -0,0 +1,14 @@
+/* stats */
+/*
+#ifndef BOOL_DEFINE
+#define BOOL_DEFINE
+typedef enum { false, true } bool;
+#endif
+*/
+void stats_prefix_init(void);
+void stats_prefix_clear(void);
+void stats_prefix_record_get(const char *key, const size_t nkey, const bool is_hit);
+void stats_prefix_record_delete(const char *key, const size_t nkey);
+void stats_prefix_record_set(const char *key, const size_t nkey);
+/*@null@*/
+char *stats_prefix_dump(int *length);
